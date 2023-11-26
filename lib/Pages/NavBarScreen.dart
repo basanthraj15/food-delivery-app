ecf6861c38f69854df2f3f1d312b267c6315a408
basanthraj15 <basanthraj.2002@gmail.com>
2023-11-26 13:00:28 +0530

    NavBar  added

--- lib/Pages/NavBarScreen.dart
+++ lib/Pages/NavBarScreen.dart
@@ -1,10 +1,130 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/Pages/HomePage.dart';
+import 'package:food_delivery/Pages/LoginScreen.dart';
+
 
 class NavBar extends StatelessWidget {
   const NavBar({super.key});
 
+  get currentAccountPicture => null;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          },
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Container(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text('Account Name'),
+              accountEmail: Text('Email id'),
+              currentAccountPicture: CircleAvatar(
+                child: ClipOval(
+                  child: Image.asset(
+                    'assets/images/User.png',
+                    width: 90,
+                    height: 90,
+                    fit: BoxFit.cover,
+                    /* loadingBuilder:(context,child,LoadingProgress)   {
+              return CircularProgressIndicator();
+            }      */
+                  ),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/must.jpg',
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.verified_user_rounded),
+              title: Text('User Profile'),
+              onTap: () => null,
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.notifications),
+              title: Text('Notifications'),
+              onTap: () => null,/* Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => NotificationScreen()),
+              ), */
+              trailing: ClipOval(
+                child: Container(
+                    color: Colors.red,
+                    width: 20,
+                    height: 20,
+                    child: Center(
+                      child: Text(
+                        '5',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                    )),
+              ),
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () => null,/* Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SettingsScreen()),
+              ), */
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.description),
+              title: Text('Policies'),
+              onTap: () => null,
+            ),
+            Divider(),
+            ListTile(
+                leading: Icon(Icons.download),
+                title: Text('Download App'),
+                onTap:null, /* () async {                 
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => DownloadScreen()),
+                  );
+                } */),
+            Divider(),
+            ListTile(
+                leading: Icon(Icons.exit_to_app),
+                title: Text('Logout'),
+                onTap:() async {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()),
+                  );
+                }),
+          ],
+        ),
+      ),
+    );
   }
 }
+/* } */
