5499853d2e3be26fd15d3cdf5863d164e722b21a
basanthraj15 <basanthraj.2002@gmail.com>
2024-01-27 00:27:34 +0530

    payment gateway packages added

--- lib/Pages/PaymentScreen.dart
+++ lib/Pages/PaymentScreen.dart
@@ -7,43 +7,34 @@ class PaymentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          height: 45,
-          width: 300,
-          color: Colors.red,
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 40.0),
-                  child: Image.asset(
-                    'assets/lock.png',
-                    height: 20,
-                  ),
-                ),
-              ),
-              TextButton(
-                  onPressed: () async {
-                    /* Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            const VerifyNumber()),
-                                  ); */
-                  },
-                  child: Text(
-                    'Make Payment',
-                    style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w700),
-                  )),
-            ],
+        body: SafeArea(
+            child: Container(
+      height: 300,
+      alignment: Alignment.center,
+      padding: EdgeInsets.all(20),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 80,
           ),
-        ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
+            },
+            child: Text("Make Payment"),
+            style: ElevatedButton.styleFrom(
+              fixedSize: Size(280, 45),
+              backgroundColor: Colors.red,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+          ),
+        ],
       ),
-    );
+    )));
   }
 }
